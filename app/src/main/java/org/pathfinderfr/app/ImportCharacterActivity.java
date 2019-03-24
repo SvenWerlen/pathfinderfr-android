5e553d80fdee789056b6423fc465e0831c6b0870
Sven Werlen <sven.werlen@desjardins.com>
2019-03-24 11:47:39 -0400

    Import improvments

--- app/src/main/java/org/pathfinderfr/app/ImportCharacterActivity.java
+++ app/src/main/java/org/pathfinderfr/app/ImportCharacterActivity.java
@@ -1,17 +1,24 @@
 package org.pathfinderfr.app;
 
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.pathfinderfr.R;
+import org.pathfinderfr.app.character.CharacterSheetActivity;
 import org.pathfinderfr.app.database.DBHelper;
 import org.pathfinderfr.app.database.entity.Character;
+import org.pathfinderfr.app.database.entity.CharacterFactory;
 import org.pathfinderfr.app.database.entity.CharacterImportExport;
 import org.pathfinderfr.app.util.ConfigurationUtil;
 import org.pathfinderfr.app.util.Pair;
@@ -30,18 +37,43 @@ public class ImportCharacterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_character);
 
-        String content = handleIntent();
+        String content = handleIntent(true);
         WebView webView = findViewById(R.id.importCharacterInfo);
         webView.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
         webView.setBackgroundColor(Color.TRANSPARENT);
+
+        findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.importButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // apply import
+                ImportCharacterActivity.this.handleIntent(false);
+                // remove preselected character (if any) and target to list of characters
+                PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit()
+                        .remove(CharacterSheetActivity.PREF_SELECTED_CHARACTER_ID)
+                        .putString(MainActivity.KEY_CUR_FACTORY, CharacterFactory.FACTORY_ID)
+                        .apply();
+                // open character sheet
+                Intent intent = new Intent(ImportCharacterActivity.this, MainActivity.class);
+                startActivity(intent);
+                // finish import (avoid going back)
+                finish();
+            }
+        });
     }
 
-    private String handleIntent() {
+    private String handleIntent(boolean simulate) {
         StringBuffer content = new StringBuffer();
 
         Uri uri = getIntent().getData();
         if (uri == null) {
-            tellUserThatCouldntOpenFile();
+            content.append("<font color=\"red\">" + getString(R.string.importcharacter_error_file) + "</font>");
             return content.toString();
         }
         content.append(getString(R.string.importcharacter_info_acces)).append("<br/>");
@@ -56,7 +88,7 @@ public class ImportCharacterActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         if (text == null) {
-            tellUserThatCouldntOpenFile();
+            content.append("<font color=\"red\">" + getString(R.string.importcharacter_error_file) + "</font>");
             return content.toString();
         }
         content.append(getString(R.string.importcharacter_info_read)).append("<br/>");
@@ -65,10 +97,13 @@ public class ImportCharacterActivity extends AppCompatActivity {
                 CharacterImportExport.importCharacterAsYML(text, findViewById(android.R.id.content));
 
         if(result == null) {
+            content.append("<font color=\"red\">" + getString(R.string.importcharacter_error_parsing) + "</font>");
             return content.toString();
         }
         content.append(getString(R.string.importcharacter_info_parse)).append("<br/>");
-        DBHelper.getInstance(getApplicationContext()).insertEntity(result.first);
+        if(!simulate) {
+            DBHelper.getInstance(getApplicationContext()).insertEntity(result.first);
+        }
         content.append(getString(R.string.importcharacter_info_import)).append("<br/>");
 
         if(result.second.size() > 0) {
@@ -82,10 +117,6 @@ public class ImportCharacterActivity extends AppCompatActivity {
         content.append("<pre>").append(text).append("</pre>");
 
         return content.toString();
-    }
-
-    private void tellUserThatCouldntOpenFile() {
-        Toast.makeText(this, getString(R.string.importcharacter_error_file), Toast.LENGTH_LONG).show();
     }
 
     public static String getStringFromInputStream(InputStream stream) throws IOException {
